c9d14e1735507619754455b4a8a0d90c1459dc07
Sebastian Zambrano <50499778+SebastianZambranoR@users.noreply.github.com>
2022-04-05 23:40:03 -0500

    CharacterUpdate

--- Taller2/Testss/Equip.cs
+++ Taller2/Testss/Equip.cs
@@ -8,10 +8,21 @@ namespace Testss
 {
      class Equip: Card
     {
-        public int targetAttribute;
-        public int effectPoints;
-        public Equip(int costPoints, string name, Card_Rarity rarity) : base(costPoints, name, rarity)
+        public enum Target_Attribute {AP, RP, ALL };
+        public enum E_Affinity { Knight, Mage, Undead, All };
+        private E_Affinity affinity;
+        private Target_Attribute target_Attribute;
+        int effectPoints;
+
+        internal E_Affinity Affinity { get => affinity; }
+        internal Target_Attribute Target_Attribute1 { get => target_Attribute;}
+        public int EffectPoints { get => effectPoints;}
+
+        public Equip(int costPoints, string name, Card_Rarity rarity, Target_Attribute target_Attribute, int effectPoints, E_Affinity affinity) : base(costPoints, name, rarity)
         {
+            this.target_Attribute = target_Attribute; 
+            this.effectPoints = effectPoints;
+            this.affinity = affinity; 
         }
     }
 }
---
+++ Taller2/Testss/IEquip.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testss
+{
+    internal interface IEquip
+    {
+        void AddEquip(Equip equip);
+        void DestroyEquip(Equip equip);
+    }
+}
